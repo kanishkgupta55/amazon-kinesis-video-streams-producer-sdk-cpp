36b9e3ec1965094ec5604caac1e40b05c6444acf
kanishkgupta55 <40593695+kanishkgupta55@users.noreply.github.com>
2018-07-10 16:12:38 +0530

    Update kvs_producer_plugin_rtsp_demo.cpp

--- kinesis-video-gstreamer-plugin/demo-src/kvs_producer_plugin_rtsp_demo.cpp
+++ kinesis-video-gstreamer-plugin/demo-src/kvs_producer_plugin_rtsp_demo.cpp
@@ -20,7 +20,7 @@ int gstreamer_init(int, char **);
 #define ACCESS_KEY_ENV_VAR "AWS_ACCESS_KEY_ID"
 #define SECRET_KEY_ENV_VAR "AWS_SECRET_ACCESS_KEY"
 #define SESSION_TOKEN_ENV_VAR "AWS_SESSION_TOKEN"
-#define DEFAULT_REGION_ENV_VAR "AWS_DEFAULT_REGION"
+#define DEFAULT_REGION_ENV_VAR "us-east-2"
 #define MAX_URL_LENGTH 65536
 
 typedef struct _CustomData {
